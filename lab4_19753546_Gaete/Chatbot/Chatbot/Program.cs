ca029c8516c0ae691a370db88dcd6b517d2dfd05
eliass1997ok <gabriel.gaete.l@usach.cl>
2018-09-03 23:15:04 -0300

    New Window working.

--- lab4_19753546_Gaete/Chatbot/Chatbot/Program.cs
+++ lab4_19753546_Gaete/Chatbot/Chatbot/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Gtk;
 
-namespace ChatbotFrontEnd
+namespace ChatbotFrontend
 {
     class MainClass
     {
---
+++ lab4_19753546_Gaete/Chatbot/Chatbot/RateWindow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Gtk;
+namespace ChatbotFrontend
+{
+    public partial class RateWindow : Gtk.Window
+    {
+        String userRate;
+        String chatbotRate;
+
+        public RateWindow() :
+                base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
+        }
+
+        protected void OnSubmitClicked(object sender, EventArgs e)
+        {
+            this.userRate = combobox3.ActiveText;
+            this.chatbotRate = combobox1.ActiveText;
+        }
+
+        public String getUserRate(){
+            return this.userRate;
+        }
+
+        public String getChatbotRate(){
+            return this.chatbotRate;
+        }
+    }
+}
